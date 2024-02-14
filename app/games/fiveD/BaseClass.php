673f981f18c0d8a2f4f3d186fdce88fa4fa09944
= <=>
2024-02-14 20:39:36 +0800

    hmm

--- app/games/fiveD/BaseClass.php
+++ app/games/fiveD/BaseClass.php
@@ -59,7 +59,21 @@ class BaseClass {
         $drawNumber == $selection ? true : false; #-> fn([1,1,2,2,3],[[1,2,3,4]],[1,1,1,1],5) = true
     }
 
+    public static function group12(Array $drawNumber, Array $selection, Array $pattern, Int $slice): mixed { // group 12
+        $val = array_count_values($drawNumber); unset($selection[1][max($val)]);
+        return Utils::findPattern($pattern, $drawNumber, 0, $slice) && 
+        in_array(array_search(max($val), $val), $selection[0]) &&
+        count(array_intersect($selection[1], $drawNumber)) == 2; #-> fn([1,1,2,3],[[1,2],[2,3]],[2,1,1],4) = true
+    }
+
+    public static function group6(Array $drawNumber, Array $selection, Array $pattern, Int $slice): mixed { // group 6
+        $val = array_count_values($drawNumber);
+        return Utils::findPattern($pattern, $drawNumber, 0, $slice) && 
+        in_array(array_search(max($val), $val), $selection[0]) && 
+        count(array_intersect(array_diff($selection[1], [array_search(max($val), $val)]), $drawNumber)) == 1; #-> fn([1,1,2,3],[[1,2],[2,3]],[2,1,1],4) = true
+    }
+
 }
 
 
-var_dump(BaseClass::group24([1,1,2,2,3],[1,2,3,4],[1,1,1,1],4));
+var_dump(BaseClass::group12([1,1,2,3],[[1,2],[2,3]],[2,2],4));
