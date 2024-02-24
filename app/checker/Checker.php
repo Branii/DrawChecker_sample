83c3f406c0f97faa735a45bcea8fa147b7b08173
Nii Armar Adjaidoo <braniiblack@gmail.com>
2024-02-24 22:01:52 +0000

    checker hacked

--- app/checker/Checker.php
+++ app/checker/Checker.php
@@ -2,17 +2,17 @@
 ini_set('memory_limit', '256M');
 class Checker { // checker class
 
-  public static function Check(Array $gameIDs, String $queueName) { # recieve game ids from gamesIdMap class
+  public static function Check(Array $gameIds, String $queueName) { # recieve game ids from gamesIdMap class
 
       try {
-        
-        foreach ($gameIDs as $gameID) {
 
-          $gameDrawInfo = Utils::getdrawfromapi($gameID);
+        foreach ($gameIds as $gameId) {
+
+          $gameDrawInfo = Utils::getdrawfromapi($gameId);
           $workLoad = [
             'drawNumber' => $gameDrawInfo['draw_number'], 
             'betPeriod'  => $gameDrawInfo['draw_date'], 
-            'betTable'   => GameTableMap::getGameTableMap()[$gameID]['bet_table']
+            'betTable'   => GameTableMap::getGameTableMap()[$gameId]['bet_table']
           ];
 
           echo (new QueueProducer(Config::getQueueServerAddress()))->addJobToQueue($queueName, json_encode($workLoad)); // send to beanstalk queue
