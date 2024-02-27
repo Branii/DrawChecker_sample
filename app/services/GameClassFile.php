455124afa5af353bc92c979582a7fd3052d3e606
= <=>
2024-02-27 12:55:25 +0800

    update

--- app/services/GameClassFile.php
+++ app/services/GameClassFile.php
@@ -6,7 +6,7 @@ class GameClassFile {
            '1,4,5,6,7,8,9' => 'FiveD',
            '3,17,23'       => 'Pk10',
            '10,11,12'      => 'Fast3',
-           '16'            => 'ThreeD',
+           '16,30'            => 'ThreeD',
            '27,28'         => 'Eleven5',
            '25,26'         => 'Mark6',
            '29'            => 'Happy8',
