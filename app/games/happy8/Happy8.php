c2cc55c555e057bc023d5afe5ac9604fbda99e03
= <=>
2024-03-01 10:40:20 +0800

    hppy8

---
+++ app/games/happy8/Happy8.php
@@ -0,0 +1,54 @@
+<?php 
+
+class Happy8 extends GamePlayFunctionHappy8 { 
+
+    public static function PickOne(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 1;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10,...20]) = true
+
+    public static function PickTwo(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 2;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10...20]) = true
+
+    public static function PickThree(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 3;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10...20]) = true
+
+    public static function PickFour(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 4;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10...20]) = true
+
+    public static function PickFive(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 5;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10...20]) = true
+
+    public static function PickSix(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 6;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10...20]) = true
+
+    public static function PickSeven(Array $selection, Array $drawNumber) : Bool {
+        $newArray = array_merge($selection[0], $selection[1]);
+        return count(array_intersect($drawNumber, $newArray)) >= 7;
+    } //-> fn([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10...20]) = true
+
+
+    // happy 8 fun ------------------------------
+    public static function FunOverUnder(Array $selection, Array $drawNumber) : Bool {
+      return false;
+    } 
+
+    public static function FunOddEven(Array $selection, Array $drawNumber) : Bool {
+      return false;
+    }
+
+    public static function FunSum(Array $selection, Array $drawNumber) : Bool {
+      return false;
+    }
+
+}
--- app/games/pk10/GamePlayFunctionPk10.php
+++ app/games/pk10/GamePlayFunctionPk10.php
@@ -1,5 +1,33 @@
 <?php 
 
 class GamePlayFunctionPk10 {
-    
-}
+
+    public static function getGamePlayFunction() : Array {  // Game Ids and their respective checker function
+      return  [
+
+        #---------------Pk10 Standard----------------
+
+           '469' => 'f3straightJoint',
+           '470' => 'f3straightManual',
+           '474' => 'f3groupJoint',
+           '475' => 'f3straightManual',
+           '476' => 'f2straightJoint',
+           '477' => 'f2straightManual',
+           '478' => 'f2groupJoint',
+           '479' => 'f2groupManual',
+           '480' => 'anyPlace',
+           '481' => 'fixedPlace',
+           '482' => 'pickJoint1x1', 
+           '484' => 'pickJoint2x2',
+           '486' => 'pickJoint3x3',
+           '488' => 'pickJoint4x4',
+           '490' => 'pickJoint5x5',
+           '492' => 'pickJoint5x6',
+           '494' => 'pickJoint5x7',
+           '496' => 'pickJoint5x8',
+
+        #---------------11x5 2Sides----------------=> waiting for 2side game ids
+
+      ]; 
+    }
+ }
