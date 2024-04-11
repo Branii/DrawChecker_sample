2637e505ffc6beffdf07356c65efe2b0f362e6cb
Emmanuel Adjaidoo Nii - Armar <Braniiblack@gmail.com>
2024-04-11 09:17:47 +0800

    Update QueueConsumer.php

--- app/services/QueueConsumer.php
+++ app/services/QueueConsumer.php
@@ -52,13 +52,13 @@ class QueueConsumer { // queue the game draws
 //(new QueueConsumer(Config::getQueueServerAddress()))->QueueExecutionProcess('queue25'); Testing for 1 queue
 
 // Start a queue for each job in a separate process
-foreach (QueueHolders::getQueueContainer() as $queueName) { // QueueHolders::getQueueContainer() is a static method that returns an array of queues and their queuing method
-    (new QueueConsumer(Config::getQueueServerAddress()))->startConsumers($queueName);
-    echo "Started QueueHolder for $queueName" . "\n";
-}
+// foreach (QueueHolders::getQueueContainer() as $queueName) { // QueueHolders::getQueueContainer() is a static method that returns an array of queues and their queuing method
+//     (new QueueConsumer(Config::getQueueServerAddress()))->startConsumers($queueName);
+//     echo "Started QueueHolder for $queueName" . "\n";
+// }
 //echo "QueueContainers Started ...";
 // Wait for all child processes to exit
-while (pcntl_waitpid(0, $status) != -1);
+//while (pcntl_waitpid(0, $status) != -1);
 
 
 
